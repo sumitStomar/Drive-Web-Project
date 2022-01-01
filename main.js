369e75d194eb8414251013c1f87b2502d8ec14d2
Sumit Singh Tomar <tomarsumit570@gmail.com>
2022-01-01 20:15:08 +0530

    first

---
+++ main.js
@@ -0,0 +1,25 @@
+(function () {
+    let btnAddFolder = document.querySelector("#btnAddFolder");
+
+    let divContainer = document.querySelector("#divContainer");
+
+    let pageTemplates = document.querySelector("#pageTemplates");
+
+    btnAddFolder.addEventListener("click", function () {
+        let fname = prompt("Folder name?");
+        if (fname == null) {
+            return;
+        }
+
+        let divFolderTemplate = pageTemplates.content.querySelector(".folder");
+        let divFolder = document.importNode(divFolderTemplate, true);
+
+        let divName = divFolder.querySelector("[purpose ='name']");
+        divName.innerHTML = fname;
+
+        divContainer.appendChild(divFolder);
+        
+
+    });
+
+})();
